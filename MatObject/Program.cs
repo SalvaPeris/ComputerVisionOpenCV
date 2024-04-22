4c594f2460d37a82403b77e0703cea9387af9784
SalvaPeris <sperisgimeno@gmail.com>
2024-04-22 12:25:31 +0200

    Added Mat creation object project.

---
+++ MatObject/Program.cs
@@ -0,0 +1,15 @@
+﻿using OpenCvSharp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is a demo of Mat creation: ");
+        Mat mat = new Mat(30, 40, MatType.CV_8UC3, new Scalar(0, 0, 255));
+        Mat mat1 = new Mat(140, 130, MatType.CV_8UC3, new Scalar(0, 255, 0));
+        Cv2.ImShow("m", mat);
+        Cv2.ImShow("m1", mat1);
+        Cv2.WaitKey();
+        Cv2.DestroyAllWindows();
+    }
+}
