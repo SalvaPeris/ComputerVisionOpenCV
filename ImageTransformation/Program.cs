40a1d6cb5935856c5682ea3cc8b34582ed46ace0
SalvaPeris <sperisgimeno@gmail.com>
2024-04-22 12:14:26 +0200

    Agregar archivos de proyecto.

---
+++ ImageTransformation/Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenCvSharp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is a demo for images transformation:");
+
+        string fileName = "lena.jpg";
+        Mat image = Cv2.ImRead(fileName, ImreadModes.Grayscale);
+        Cv2.ImShow("lena", image);
+        Cv2.ImWrite(@"../../../images/grayLena.jpg", image);
+        Cv2.WaitKey();
+        Cv2.DestroyAllWindows();
+    }
+}
